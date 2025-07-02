e73b6a761c8abf950f1452d01cd50cc06f63fe19
Pearl Oyewole <pearloyewole1@gmail.com>
2025-07-02 21:04:50 +0000

    done

--- src/dither.ml
+++ src/dither.ml
@@ -2,31 +2,57 @@ open Core
 
 let transform image =
   let grayscale_image = Grayscale.transform image in
-  let max_pixel_value = Image.max_val grayscale_image in 
-  let new_image = Image.mapi grayscale_image ~f:(fun ~x ~y pixel ->
-    let brightness = Pixel.red pixel in
-    let error = max_pixel_value - brightness
-
-    if brightness > max_pixel_value/2  
-        then Image.set grayscale_image ~x ~y (max_pixel_value,max_pixel_value,max_pixel_value)
-    else Image.set grayscale_image ~x ~y Pixel.zero 
-
-    let right_pixel_addition = ((7/.16) *. error, (7/.16) *. error, (7/.16) *. error) in 
-    let left_pixel_addition = ((3/.16) *. error, (3/.16) *. error, (3/.16) *. error) in 
-    let down_pixel_addition = ((5/.16) *. error, (3/.16)*. error, (3/.16)*. error)in 
-    let diagonal_pixel_addition = ((1/.16) *. error, (1/.16) *. error, (1/.16) *. error) in 
-
-    let current_pixel ~x ~y = Image.get grayscale_image ~x ~y 
-
-    Image.set grayscale_image ~x+1 ~y Pixel.(+) current_pixel right_pixel_addition
-    Image.set grayscale_image ~x+1 ~y Pixel.(+) current_pixel left_pixel_addition
-    Image.set grayscale_image ~x+1 ~y Pixel.(+) current_pixel down_pixel_addition
-    Image.set grayscale_image ~x+1 ~y Pixel.(+) current_pixel diagonal_pixel_additio
-
+  let max_pixel_value = Image.max_val grayscale_image in
+  let new_image =
+    Image.mapi grayscale_image ~f:(fun ~x ~y pixel ->
+      let brightness = Pixel.red pixel in
+      let error = max_pixel_value - brightness in
+      let new_pixel =
+        if brightness > max_pixel_value / 2
+        then max_pixel_value, max_pixel_value, max_pixel_value
+        else Pixel.zero
+      in
+      let right_addition = int_of_float (7. /. 16. *. float_of_int error) in
+      let right_pixel_addition =
+        right_addition, right_addition, right_addition
+      in
+      let d_left_addition = int_of_float (3. /. 16. *. float_of_int error) in
+      let d_left_pixel_addition =
+        d_left_addition, d_left_addition, d_left_addition
+      in
+      let down_addition = int_of_float (5. /. 16. *. float_of_int error) in
+      let down_pixel_addition =
+        down_addition, down_addition, down_addition
+      in
+      let d_right_addition =
+        int_of_float (1. /. 16. *. float_of_int error)
+      in
+      let d_right_pixel_addition =
+        d_right_addition, d_right_addition, d_right_addition
+      in
+      Image.set
+        grayscale_image
+        ~x:(x + 1)
+        ~y
+        (Pixel.( + ) pixel right_pixel_addition);
+      Image.set
+        grayscale_image
+        ~x:(x - 1)
+        ~y:(y - 1)
+        (Pixel.( + ) pixel d_left_pixel_addition);
+      Image.set
+        grayscale_image
+        ~x
+        ~y:(y - 1)
+        (Pixel.( + ) pixel down_pixel_addition);
+      Image.set
+        grayscale_image
+        ~x:(x + 1)
+        ~y:(y - 1)
+        (Pixel.( + ) pixel d_right_pixel_addition);
+      new_pixel)
+  in
   new_image
-
-  (*change new image vs grayscale image*)
-
 ;;
 
 let command =
